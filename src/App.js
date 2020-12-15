b88b49f479e5eeba912240c9f49c2104484c8793
Giuseppe <giuseppe.ilsami@gmail.com>
2020-12-15 17:45:59 +0100

    app.js e css aggiornati

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import DateNav from './components/DateNav';
 import CurrentWeather from './components/CurrentWeather';
 import ForecastWeather from './components/ForecastWeather'
 
 function App() {
-  const logoUrl = "https://lh3.googleusercontent.com/proxy/_qnRNlAxG2-50zMxrHiUq4dMJKRcWla1SKehmtSE0r6Xssrhy7J0phsawu9PJytuZwI1XdaGGkqxnQmP9Z2k_vZF9bpJZ0FDj8yBUGAVe-ZkF34vWNZ8Si7Y63SRFtO2AbGdjaxUWqOzvSw6MtOn"
+  const logoUrl = "https://upload.wikimedia.org/wikipedia/it/d/d6/Castelfranco_Emilia-Stemma.png"
   const logoHref = "https://www.comune.castelfranco-emilia.mo.it/servizi/notizie/notizie_homepage.aspx";
   return (
     <>
@@ -12,7 +11,6 @@ function App() {
         <a href={`${logoHref}`}>
           <img src={`${logoUrl}`} alt="Castelfranco Emilia logo"></img>
         </a>
-        <DateNav />
       </nav>
       <main className="contenitore">
         <CurrentWeather />
